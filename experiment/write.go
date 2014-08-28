86b3aab5f6587e206d5eeb6fc8997b38fb67bb9e
Ian Hansen <ihansen@digitalocean.com>
2014-08-28 15:39:29 -0400

    gets map type

--- experiment/write.go
+++ experiment/write.go
@@ -12,29 +12,39 @@ import (
 const RiakAddr = "104.131.63.89:8087"
 
 func main() {
-	req := &riak.RpbGetBucketReq{
-		Bucket: []byte("sldkfjsltats.droplet.1234.cpu"),
-		Type:   []byte("metricgroup"),
-	}
+	// establish conn
 	conn, err := griak.NewConn(RiakAddr)
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	// make request payload
+	req := &riak.RpbGetReq{
+		Bucket: []byte("stats.droplet.1234.cpu"),
+		Key:    []byte("1409230800000"),
+		Type:   []byte("metricgroup"),
 	}
 	data, err := proto.Marshal(req)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = conn.Write(19, data)
+	// write request
+	var code byte = 9
+	err = conn.Write(code, data)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	_, data, err = conn.Read()
+	// read request
+	code, data, err = conn.Read()
 	if err != nil {
 		log.Fatal(err)
 	}
-	resp := &riak.RpbGetBucketResp{}
+	if code != 10 {
+		log.Fatalf("expected message code 10 not %d", code)
+	}
+	resp := &riak.RpbGetResp{}
 	err = proto.Unmarshal(data, resp)
 	if err != nil {
 		log.Fatal(err)
