1fd5f070d736d9a9113dab330bb9ed2ed8535dd8
Ian Hansen <ihansen@digitalocean.com>
2014-08-28 16:39:03 -0400

    adds very application specific non-forgiving map fn

--- conn.go
+++ conn.go
@@ -1,8 +1,13 @@
 package griak
 
 import (
+	"fmt"
 	"io"
 	"net"
+
+	"code.google.com/p/goprotobuf/proto"
+
+	"github.com/supershabam/griak/pb/riak"
 )
 
 type Conn struct {
@@ -15,6 +20,41 @@ func NewConn(addr string) (*Conn, error) {
 		return nil, err
 	}
 	return &Conn{conn: conn}, nil
+}
+
+func (c Conn) ReadMap(types, buckets, datatypes string) (map[string]string, error) {
+	req := &riak.DtFetchReq{
+		Type:   []byte(types),
+		Bucket: []byte(buckets),
+		Key:    []byte(datatypes),
+	}
+	data, err := proto.Marshal(req)
+	if err != nil {
+		return nil, err
+	}
+	err = c.Write(80, data)
+	if err != nil {
+		return nil, err
+	}
+	code, data, err := c.Read()
+	if err != nil {
+		return nil, err
+	}
+	if code != 81 {
+		return nil, fmt.Errorf("expected code 81 not: %d", code)
+	}
+	resp := &riak.DtFetchResp{}
+	err = proto.Unmarshal(data, resp)
+	if err != nil {
+		return nil, err
+	}
+	result := make(map[string]string)
+	for _, mapEntry := range resp.GetValue().GetMapValue() {
+		key := string(mapEntry.GetField().GetName())
+		value := string(mapEntry.GetRegisterValue())
+		result[key] = value
+	}
+	return result, nil
 }
 
 func (c Conn) Read() (code byte, data []byte, err error) {
--- experiment/write.go
+++ experiment/write.go
@@ -3,10 +3,7 @@ package main
 import (
 	"log"
 
-	"code.google.com/p/goprotobuf/proto"
-
 	"github.com/supershabam/griak"
-	"github.com/supershabam/griak/pb/riak"
 )
 
 const RiakAddr = "104.131.63.89:8087"
@@ -18,37 +15,10 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// make request payload
-	req := &riak.DtFetchReq{
-		Bucket: []byte("stats.droplet.1234.cpu"),
-		Key:    []byte("1409230800000"),
-		Type:   []byte("metricgroup"),
-	}
-	data, err := proto.Marshal(req)
+	m, err := conn.ReadMap("metricgroup", "stats.droplet.1234.cpu", "1409230800000")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// write request
-	var code byte = 80
-	err = conn.Write(code, data)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// read request
-	code, data, err = conn.Read()
-	if err != nil {
-		log.Fatal(err)
-	}
-	if code != 81 {
-		log.Fatalf("expected message code 10 not %d", code)
-	}
-	resp := &riak.DtFetchResp{}
-	err = proto.Unmarshal(data, resp)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("%+v", resp)
-
+	log.Printf("m: %+v", m)
 }
